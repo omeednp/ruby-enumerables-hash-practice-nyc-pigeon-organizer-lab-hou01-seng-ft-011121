2e8aa4589dcc5a45f6daf49e7cd99bb80c6f6f77
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-06 09:15:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,11 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   
   data.each_with_object({}) do |(key, value), list|
-    value.each do |key2, name|
-      if 
+    
+    value.each do |key2, pidgeon_name|
+      
+      pidgeon_name.each do |name|
+        
+        if list[name] 
   
 end
